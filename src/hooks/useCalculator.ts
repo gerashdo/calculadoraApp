ba548d14d319825e53abd4e8d7e0f16d00a0aafa
gerashdo <gerardo.jimenezarguelles@gmail.com>
2023-01-14 14:57:49 -0600

    Add followed operations without equals action

--- src/hooks/useCalculator.ts
+++ src/hooks/useCalculator.ts
@@ -71,20 +71,58 @@ export const useCalculator = () => {
     }
 
     const btnPlus = () => {
-        swapLastWithNumber()
+        if( lastNumber !== '0' ){
+            calculateWithoutStop()
+        }else{
+            swapLastWithNumber()
+        }
         lastOperator.current = Operators.plus
     }
     const btnSubstract = () => {
-        swapLastWithNumber()
+        if( lastNumber !== '0' ){
+            calculateWithoutStop()
+        }else{
+            swapLastWithNumber()
+        }
         lastOperator.current = Operators.substract
     }
     const btnMultiply = () => {
-        swapLastWithNumber()
+        if( lastNumber !== '0' ){
+            calculateWithoutStop()
+        }else{
+            swapLastWithNumber()
+        }
         lastOperator.current = Operators.multiply
     }
     const btnDivide = () => {
-        swapLastWithNumber()
+        if( lastNumber !== '0' ){
+            calculateWithoutStop()
+        }else{
+            swapLastWithNumber()
+        }
         lastOperator.current = Operators.divide
+    }
+
+    const calculateWithoutStop = () => {
+        const num1 = Number(lastNumber);
+        const num2 = Number(number);
+
+        switch ( lastOperator.current ) {
+            case Operators.plus:
+                setLastNumber( `${ num1 + num2 }` )
+                break;
+            case Operators.substract:
+                setLastNumber( `${ num1 - num2 }` )
+                break;
+            case Operators.multiply:
+                setLastNumber( `${ num1 * num2 }` )
+                break;
+            case Operators.divide:
+                setLastNumber( `${ num1 / num2 }` )
+                break;
+        }
+
+        setNumber('0')
     }
 
     const calculate = () => {
